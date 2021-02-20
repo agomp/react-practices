a7becac8ad208bd0c5deb02cead39e0ebbf32a02
Alberto <albertogomp@gmail.com>
2021-02-20 11:49:25 +0100

    Implemented print of values

--- src/components/List.js
+++ src/components/List.js
@@ -5,19 +5,41 @@ class List extends Component  {
     constructor(props) {
         super(props);
         this.state =  {
-            isLoading: false
+            isLoading: false,
+            videos: null
         }
     }
     componentDidMount() {
         this.setState({isLoading: true});
+        // With this, we're simulating an async post
         setTimeout(() => {
-            this.setState({isLoading: false});
+            this.setState({
+                isLoading: false,
+                videos:[{id:1},{id:2},{id:3}]
+            });
         }, 2000);
     }
     render() {
-        const { isLoading } = this.state;
-        if( isLoading ) return (<Loading message="Loading..."/>);
-        return (<div> Contenido </div>)
+        const { videos, isLoading } = this.state;
+        if( isLoading ) {
+            return (<Loading message="Loading..."/>);
+        }
+        return (
+        <React.Fragment> {/* React.Fragment prevents crashes and prepares a void object to protect the exec */}
+            <div className="container">
+                <div className="grid-container">
+                    {
+                        // map iterate over the array, JS vanilla
+                        videos && videos.map((video,i) => {
+                            return (
+                                <span> #{video.id} </span>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </React.Fragment>
+        )
     }
 }
 export default List
