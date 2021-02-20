b21ea92efaa43d67fcfa8bca59a2a6b2070f3689
Alberto <albertogomp@gmail.com>
2021-02-20 11:37:27 +0100

    Added interaction and linking components

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class Loading extends Component {
     render () {
         return (
-            <div className="loading">Loading...</div>
+            <div className="loader">{this.props.message || 'cargando...'}</div>
         )
     }
+}
+
+Loading.propTypes = {
+    message: PropTypes.string.isRequired
 }
 
 export default Loading;
