86250e4ffb1db43088b6733f3d6d8f7abdc80a38
Alberto <albertogomp@gmail.com>
2021-02-21 12:53:21 +0100

    Added links

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-const Item = ({ data }) => (
+import { Link } from 'react-router-dom'
+const Item = ({ data }) => (<Link className="grid-item-link" to={`/${data.id}`}>
     <div className="grid-item" >
         <img className="preview-image" src={data.image} alt={data.name}/>
         <div className="preview-title">{data.name}</div>
     </div>
-)
+</Link>)
 
 Item.propTypes = {
     data: PropTypes.object.isRequired
