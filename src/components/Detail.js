8a0cd3deb4a2051d8e1feb5e504c9a917923d683
Alberto <albertogomp@gmail.com>
2021-02-22 22:26:17 +0100

    Added external JS  Vanilla function

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { withRouter } from 'react-router'
 import { getVideoDetail } from '../api';
 import Loading from './Loading';
 import Video from './Video';
+import FadeIn from './FadeIn'
 
-class Detail extends Component {
+class Detail extends PureComponent {
     // eslint-disable-next-line no-useless-constructor
     constructor(props) {
         super(props);
@@ -17,7 +18,7 @@ class Detail extends Component {
         this.setState({isLoading: true});
         getVideoDetail({idVideo: match.params.id })
         .then(data => this.setState({ video: data, isLoading: false}))
-        .catch(err => this.setState({ error: err, isLoading: false}));
+        .catch(err => this.setState({ error: err, isLoading: false})); 
     }
     render() {
         const { isLoading, error, video} = this.state;
@@ -29,7 +30,7 @@ class Detail extends Component {
             <div className="detail-container">
                 <Video title={video.title} embed={video.embed}/>
                 <div className="detail-summary">
-                    <h2 className="detail-title">{video.title}</h2>
+                    <h2 key={video.id} className="detail-title" ref={el => FadeIn(el) }>{video.title}</h2>
                     <p>{video.description}</p>
                 </div>
             </div>
